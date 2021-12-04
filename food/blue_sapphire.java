64ea5b96cc63ad1e144bb0dbbb53298af35bf9d7
minseoooo <95116664+minseoooo@users.noreply.github.com>
2021-12-04 17:46:32 +0900

    Add files via upload

---
+++ food/blue_sapphire.java
@@ -0,0 +1,14 @@
+package com.example.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class blue_sapphire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_sapphire);
+    }
+}
