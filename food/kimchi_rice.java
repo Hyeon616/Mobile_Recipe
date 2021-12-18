d706522859a4c32c6cd4d59a5aeff7f22c25568e
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2021-12-19 02:57:41 +0900

    timer
    타이머 구현 및 오타수정

--- food/kimchi_rice.java
+++ food/kimchi_rice.java
@@ -2,13 +2,36 @@ package com.example.recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class kimchi_rice extends AppCompatActivity {
+
+    // 타이머 선언
+    Button startTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kimchi_rice);
+
+        // 타이머 액티비티 버튼 id 설정
+        startTime =(Button) findViewById(R.id.startTimer);
+
+        // 버튼누르면 타이머 실행
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Timer.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
